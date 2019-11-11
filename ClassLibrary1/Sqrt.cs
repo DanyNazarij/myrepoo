f5e21116d3b24c1eedd8d4dab5ba48460a8baed5
AndrewKalushka <andrew.kalushka@gmail.com>
2019-11-11 11:40:17 +0200

    fff

---
+++ ClassLibrary1/Sqrt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Sqrt
+    {
+        public static double[] Calc(double a, double b, double c)
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+            double[] result = new double[]
+            {
+                (-b + Math.Sqrt(d)) / (2 * a), (-b - Math.Sqrt(d)) / (2 * a)
+            };
+            return result;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using ClassLibrary1;
+
+namespace Test
+{
+
+    [TestFixture]
+        public class UnitTest1
+        {
+            [Test]
+            public void TestMethod1()
+            {
+                double a = 1;
+                double b = -5;
+                double c = 4;
+                double[] x1 = { 4, 1 };
+                double[] x2 = Sqrt.Calc(a, b, c);
+                Assert.AreEqual(x1, x2);
+            }
+        }
+    }
+
